834f9c9f7d652421f82089d225e61f3a0e20f266
pet-r-off <pda761@mail.ru>
2023-04-18 16:41:47 +0300

    yocto_setup.sh: add lib dep for libsfml build

--- yocto_setup.sh
+++ yocto_setup.sh
@@ -19,7 +19,9 @@ sudo apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib b
 sudo apt install -y gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio \
 			python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping \
 			python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm \
-			python3-subunit mesa-common-dev zstd liblz4-tool
+			python3-subunit mesa-common-dev zstd liblz4-tool freetype* libx11-dev x11-xserver-utils \ 
+			udev mesa-utils flac libogg-dev libvorbis-dev vorbis-tools libvorbisenc2 libvorbisfile3 \
+			libalut-dev libpthread-stubs0-dev \
 
 
 echo "alias python=python3" >> ~/.bashrc
